1ca2c7ce1afaa6fcc63d478542ae58c7def883b3
BikalpaRegmi <Bikalparegmihtd3@gmail.com>
2023-12-25 09:17:32 +0545

    make it better

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -21,7 +21,7 @@ router.route('/createpost').post( authentication, async(req,res)=>{
 //for getting all posts
 router.route('/getallpost').get(async(req,res)=>{
    try {
-       const result = await Post.find().populate('postedBy' , '_id name') ;
+       const result = await Post.find().populate('postedBy' , '_id name').populate('comments.postedBy', '_id name');
        res.json(result) ;
    } catch (error) {
       res.json(error)
@@ -31,7 +31,7 @@ router.route('/getallpost').get(async(req,res)=>{
 //for getting posts by individual persons
 router.route('/mypost').get(authentication , async(req,res)=>{
    try {
-      const result = await Post.find({ postedBy : req.user._id}).populate('postedBy' , '_id name')
+      const result = await Post.find({ postedBy : req.user._id}).populate('postedBy' , '_id name').populate('comments.postedBy', '_id name')
       res.json(result)
    } catch (error) {
       res.send(error)
@@ -44,7 +44,7 @@ router.route('/likes').patch(authentication , async(req,res)=>{
    const result =  await Post.findByIdAndUpdate(req.body.postId , {
       $push : { likes : req.user._id} 
       },{new:true}
-   )
+   ).populate('likes')
    res.json(result)
    } catch (error) {
       res.send(error)
@@ -64,6 +64,20 @@ router.route('/unlike').patch(authentication , async(req,res)=>{
    }
 })
 //for comments
-router.route('/comment').patch()
+router.route('/comment').patch(authentication , async(req,res)=>{
+   try {
+      const comment = {
+         comment : req.body.comment ,
+         postedBy : req.user._id,
+      }
+      const result = await Post.findByIdAndUpdate(req.body.postId , {
+         $push : {comments : comment}
+      } , {new:true})
+      res.json(result) ;
+   } catch (error) {
+      res.send(error)
+   }
+})
+
 
 module.exports = router
